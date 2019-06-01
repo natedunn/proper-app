b366dcb614739ae4c9e6c60d33d1e2c58771db21
Nate Dunn <hello@natedunn.net>
2019-06-01 16:47:55 -0500

    Move functions and state to highter component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+// Deps
 import React, { Component } from 'react';
 // Components
 import Head from 'component/head';
 import Nav from 'component/nav';
 import Search from 'component/search';
+import Queue from 'component/queue';
 // Theme
 import styled from 'styled-components';
 import tw from 'tailwindcss/defaultTheme';
+// Imported
+import { getData } from '../utils/fetch';
+import { makeZipFile } from '../utils/helper';
 
 // ? Does this belong here?
 const pageInfo = {
@@ -18,7 +23,105 @@ const Container = styled.div`
 `;
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      origin: {},
+      queue: [],
+      results: [],
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  resetResults = () => {
+    console.log('Reset results?');
+    this.setState({
+      results: [],
+    });
+  };
+
+  resetTerm = () => {
+    this.setState({
+      term: '',
+    });
+  };
+
+  resetQueue = () => {
+    this.setState({
+      queue: [],
+    });
+  };
+
+  onOriginChange = event => {
+    const { target } = event;
+    const selectedValue = target.value;
+    const selectedText = target.options[target.selectedIndex].text;
+    if (selectedValue) {
+      this.setState({
+        origin: {
+          name: selectedText,
+          id: selectedValue,
+        },
+      });
+    }
+    this.resetResults();
+    this.resetTerm();
+  };
+
+  handleSearchChange = event => {
+    const { origin } = this.state;
+    const { value } = event.target;
+    this.setState({ term: value.trim() });
+    if (!value.trim().length) {
+      this.resetResults();
+    } else {
+      this.resetResults();
+      getData(value.trim(), origin.id).then(results => {
+        if (results && results.length) this.setState({ results });
+      });
+    }
+  };
+
+  addToQueue = item => {
+    const { origin, queue } = this.state;
+    const { name, ver } = item;
+    this.setState({
+      queue: [
+        ...queue,
+        {
+          ...queue[name],
+          name,
+          origin: origin.name,
+          ver,
+        },
+      ],
+    });
+    this.setState({
+      term: '',
+      results: [],
+    });
+  };
+
+  deleteItem = key => {
+    const { queue } = this.state;
+    this.setState({
+      queue: queue.filter((_, i) => i !== key),
+    });
+  };
+
+  createZip = manifest => {
+    const { queue } = this.state;
+    const itemNames = [];
+    queue.forEach(function(item) {
+      const { name } = item;
+      itemNames.push(name);
+    });
+    makeZipFile(manifest, itemNames.join('\n'));
+  };
+
   render() {
+    const { queue } = this.state;
     return (
       <Container>
         <Head title={pageInfo.title} />
@@ -26,8 +129,19 @@ export default class Home extends Component {
         <h1 className="text-4xl font-light text-center mt-16 mb-6">
           Work with your machine, not against it
         </h1>
-        <Search />
+        <Search
+          {...this.state}
+          addToQueue={this.addToQueue}
+          handleSearchChange={this.handleSearchChange}
+          handleSearchKeyDown={this.handleSearchKeyDown}
+          onOriginChange={this.onOriginChange}
+        />
+        <Queue
+          items={queue}
+          deleteItem={this.deleteItem}
+          createZip={this.createZip}
+        />
       </Container>
-    )
+    );
   }
 }
