f3b30d7409611f3d409e0c4e3f578683692e4f42
Nate Dunn <hello@natedunn.net>
2019-09-05 18:03:02 -0500

    Add Google font

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import App, { Container } from 'next/app';
+import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
 import tw from 'tailwindcss/defaultTheme';
 import 'style/main.css';
@@ -24,12 +25,18 @@ class MyApp extends App {
         height: 100%;
       }
       body {
-        background: #f7f9fc;
+        background-color: whitesmoke;
       }
     `;
 
     return (
       <div>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <GlobalStyle />
         <Component {...pageProps} />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: [
+        '"PT Sans"',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '“Roboto”',
+        '“Droid Sans”',
+        '“Helvetica Neue”',
+        'Helvetica',
+        'Arial',
+        '"sans-serif"',
+      ],
+    },
     extend: {},
   },
   variants: {},
